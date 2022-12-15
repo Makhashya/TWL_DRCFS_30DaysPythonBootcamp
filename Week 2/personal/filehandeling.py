2098a9ee03e039f561c31dd4fa3360215d573bae
Makhashya <makhashyaujwal@gmail.com>
2022-12-15 14:36:31 +0545

    ok

---
+++ Week 2/personal/filehandeling.py
@@ -0,0 +1,22 @@
+#open the file in read only mode
+#pwd = open('week 2/personal/ujwal.txt','r')
+#print(pwd)
+#read the contents of the file
+#actual_file = pwd.read()
+#print(actual_file)
+#pwd.close()
+
+#file = open('Week 2\personal\my_file.txt','w')
+#file.write("hello world")
+#file.close()
+
+#file = open("Week 2\\personal\\ujwal.txt",'a')
+#file.write("\nThis is an additional line.")
+#file.close()
+
+#with open("Week 2\\personal\\my_file.txt","r") as file:
+    #contents = file.read()
+    #print(contents)
+
+
+
