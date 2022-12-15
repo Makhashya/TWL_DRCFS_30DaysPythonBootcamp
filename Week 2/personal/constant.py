414a54e15a5d12a50d833f0f661779237503bcdd
Makhashya <makhashyaujwal@gmail.com>
2022-12-15 14:36:16 +0545

    ok

---
+++ Week 2/personal/constant.py
@@ -0,0 +1,9 @@
+#Details about mentors and students 
+
+current_students_count = 55
+current_mentor_count = 6
+
+# Write varuable name in capital letters for constant
+
+CURRENT_STUDENT_COUNT = 55
+CURRENT_MENTOR_COUNT = 6
